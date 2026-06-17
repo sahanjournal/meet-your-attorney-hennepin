ad8cb034902f9d0e2c1a02488d9c928ca5168eaf
Sam Rabiyah <sraby@users.noreply.github.com>
2026-06-17 12:32:48 -0400

    Merge two homepages into one page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,6 @@
-import React, { useEffect } from "react";
-import { navigate } from "gatsby";
-import { PageLayout } from "../components/PageLayout";
-import { CityProvider } from "../utils";
+import React from "react";
+import Homepage from "../components/Homepage";
 
-/**
- * The path that users will be redirected to when they visit the root URL.
- */
-const DEFAULT_REDIRECT_PATH = "/minneapolis";
+const Page = () => <Homepage />;
 
-const RedirectPage = () => {
-  const handleRedirect = () => {
-    navigate(DEFAULT_REDIRECT_PATH);
-  };
-
-  useEffect(() => {
-    handleRedirect();
-  }, []);
-
-  return (
-    <CityProvider city="minneapolis">
-      <PageLayout>
-        <section className="hero is-white is-fullheight is-flex is-justify-content-center is-align-items-center">
-          <div className="has-text-centered">
-            <button
-              className="button is-loading is-large is-white"
-              style={{ border: "none", boxShadow: "none" }}
-            >
-              {/* Invisible button just to use Bulma's loading spinner */}
-            </button>
-            <p className="eyebrow mt-3">Redirecting...</p>
-          </div>
-        </section>
-      </PageLayout>
-    </CityProvider>
-  );
-};
-
-export default RedirectPage;
+export default Page;
