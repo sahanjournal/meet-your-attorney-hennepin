cf4f61f1f8903448b050da9eefb6032340dbaa20
Sam Rabiyah <sraby@users.noreply.github.com>
2026-06-20 10:14:41 -0400

    Use "withPrefix" function from gatsby

--- src/components/Illustration.tsx
+++ src/components/Illustration.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+import { withPrefix } from "gatsby";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import classnames from "classnames";
 import { kebabCase } from "../utils";
-
-const USE_LOCAL_IMAGE_PATHS = true;
 
 export const Bobblehead: React.FC<{
   candidateName: string;
@@ -22,9 +21,7 @@ export const Bobblehead: React.FC<{
 }) => {
   const candidatePath = kebabCase(candidateName);
 
-  const imgPathPrefix = USE_LOCAL_IMAGE_PATHS
-    ? `../../illustrations/${candidatePath}`
-    : `${process.env.GATSBY_DOMAIN}${process.env.GATSBY_SLUG}/illustrations/${candidatePath}`;
+  const imgPathPrefix = withPrefix(`illustrations/${candidatePath}`);
 
   const animationDelay = startAnimationRightAway
     ? "0s"
