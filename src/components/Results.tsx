d41e5f13d9c73f2d2b1eb503fcb851a90d711c6b
Sam Rabiyah <sraby@users.noreply.github.com>
2026-06-23 16:52:52 -0400

    Adjust spacing and color of banner

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -334,19 +334,19 @@ const Results: React.FC = () => {
               </span>
             </div>
 
-            <div className="voter-reg-banner p-3">
-              <div className="deck has-text-weight-semibold has-text-left mb-4">
+            <div className="voter-reg-banner p-5">
+              <div className="deck has-text-weight-semibold has-text-left mt-0 mb-4">
                 Ready to Vote?
               </div>
 
-              <p className="eyebrow mb-2">
-                Make sure you are registered before heading to the polls
+              <p className="eyebrow mb-3">
+                Make sure you are registered before heading to the polls:
               </p>
               <div className="field is-grouped mb-5">
-                <button className="button is-link is-white">
+                <button className="button is-small is-link is-white">
                   Check my registration
                 </button>
-                <button className="button is-link is-white">
+                <button className="button is-small is-link is-white">
                   View Voter Guide
                 </button>
               </div>
